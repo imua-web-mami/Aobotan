82fe28af6cd2abb205d235682df41f1debed6d67
mami <imua.13137@gmail.comm>
2024-08-02 13:50:42 +0900

    header main-visualレスポンシブ追加

--- js/script.js
+++ js/script.js
@@ -22,6 +22,17 @@ $(window).on('load', function(){
   });
 });
 
+// header g-nav
+
+$(".menu-btn").click(function () {//ボタンがクリックされたら
+  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".menu-btn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
 
 
 Resources
