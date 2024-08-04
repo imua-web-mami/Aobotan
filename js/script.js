e2a6863929264ace22e92c9b0734b2c94a405c6b
mami <imua.13137@gmail.comm>
2024-08-04 13:10:10 +0900

    8/4about子ページ追加

--- js/script.js
+++ js/script.js
@@ -7,16 +7,6 @@ $('.accodion .menu>li').hover(function () {
 	$(this).children('.sub-menu').stop().slideUp(500);
 });
 
-//ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
-$(window).on('load', function () {
-	$('.accordion-area .contents').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
-	$(".open").each(function (index, element) { //openクラスを取得
-		var Title = $(element).children('.title'); //openクラスの子要素のtitleクラスを取得
-		$(Title).addClass('close');       //タイトルにクラス名closeを付与し
-		var Box = $(element).children('.box'); //openクラスの子要素boxクラスを取得
-		$(Box).slideDown(500);          //アコーディオンを開く
-	});
-});
 
 // header g-nav
 
@@ -43,17 +33,17 @@ document.addEventListener('DOMContentLoaded', function () {
 		speed: 6000, // トランジションのスピード（ミリ秒）
 		slidesPerView: 'auto', // 'auto'の場合、適切なCSSスタイルが必要
 		spaceBetween: 48, // スライド間の余白（px）
-		breakpoints: {
-			320: {
-				slidesPerView: 1,
-				spaceBetween: 0,
-				slidesPerGroup: 1,
-			},
-			820: {
-				slidesPerView: 2,
-				spaceBetween: 0
-			}
-		},
+		// breakpoints: {
+		// 	320: {
+		// 		slidesPerView: 1,
+		// 		spaceBetween: 0,
+		// 		slidesPerGroup: 1,
+		// 	},
+		// 	820: {
+		// 		slidesPerView: 2,
+		// 		spaceBetween: 0
+		// 	}
+		// },
 	});
 });
 
