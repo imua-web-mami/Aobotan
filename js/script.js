dfc47734fbe571608db75e70fca06ded281822d4
mami <imua.13137@gmail.comm>
2024-08-18 11:15:54 +0900

    8/18 main-visualアニメーション追加

--- js/script.js
+++ js/script.js
@@ -160,7 +160,6 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-// form 全て入力したら有効にする
 
 // form 全て入力したら送信ボタンを機能させる
 
@@ -182,4 +181,66 @@ document.addEventListener("DOMContentLoaded", function() {
       button.style.cursor = "default";
     }
   }
+});
+
+
+// fadeUpアニメーション
+
+function delayScrollAnime() {
+	var time = 0.2;//遅延時間を増やす秒数の値
+	var value = time;
+	$('.delayScroll').each(function () {
+		var parent = this;					//親要素を取得
+		var elemPos = $(this).offset().top;//要素の位置まで来たら
+		var scroll = $(window).scrollTop();//スクロール値を取得
+		var windowHeight = $(window).height();//画面の高さを取得
+		var childs = $(this).children();	//子要素を取得
+		
+		if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+			$(childs).each(function () {
+				
+				if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+					
+					$(parent).addClass("play");	//親要素にクラス名playを追加
+					$(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+					$(this).addClass("fadeUp");//アニメーションのクラス名を追加
+					value = value + time;//delay時間を増加させる
+					
+					//全ての処理を終わったらplayを外す
+					var index = $(childs).index(this);
+					if((childs.length-1) == index){
+						$(parent).removeClass("play");
+					}
+				}
+			})
+		}else {
+			$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
+			value = time;//delay初期値の数値に戻す
+		}
+	})
+}
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function(){
+  delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+});
+
+// fadeInアニメーション
+
+function slideAnime() {
+	$('.js-fadeIn').each(function () { //js-fadeInというクラス名が
+		var elemPos = $(this).offset().top - 50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			$(this).addClass('fadeIn');// 画面内に入ったらfadeInというクラス名を追記
+		} else {
+			$(this).removeClass('fadeIn');// 画面外に出たらfadeInというクラス名を外す
+		}
+	});
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	slideAnime();/* アニメーション用の関数を呼ぶ*/
 });
